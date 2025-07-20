befc14ee9b08f71a85dd88adc64d231067f2ffab
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-07-20 23:17:08 +0530

    Update 136.java

--- Java_LC/136.java
+++ Java_LC/136.java
@@ -9,3 +9,9 @@ class Solution {
         return res;        
     }
 }
+
+What is XOR?
+XOR (eXclusive OR) is a binary operation that compares two bits and outputs a result based on the following rules:
+
+Same bits: 0 ^ 0 = 0, 1 ^ 1 = 0
+Different bits: 0 ^ 1 = 1, 1 ^ 0 = 1
